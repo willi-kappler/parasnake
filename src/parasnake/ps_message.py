2443c0c9cf796fdd83e031c9d78b914e7192f3a5
Willi Kappler <willi.kappler.gm@gmail.com>
2024-08-26 19:53:11 +0200

    PS prefix for string constants, implement some gen_message() helper functions.

--- src/parasnake/ps_message.py
+++ src/parasnake/ps_message.py
@@ -9,37 +9,52 @@ This module defines all the messages and how to decode and encode them.
 
 
 from typing import Any
+import pickle
+import lzma
+
 
 from parasnake.ps_nodeid import PSNodeId
 
 
-HEARTBEAT_OK = "heartbeat_ok"
-HEARTBEAT_ERROR = "heartbeat_error"
-QUIT = "quit"
-INIT_OK = "init_ok"
-INIT_ERROR = "init_error"
-NEW_DATA_FROM_SERVER = "new_data_from_server"
+PS_HEARTBEAT_MESSAGE = "heartbeat_from_node"
+PS_HEARTBEAT_OK = "heartbeat_ok"
+PS_HEARTBEAT_ERROR = "heartbeat_error"
+PS_QUIT = "quit"
+PS_INIT_MESSAGE = "init_from_node"
+PS_INIT_OK = "init_ok"
+PS_INIT_ERROR = "init_error"
+PS_NEW_DATA_FROM_SERVER = "new_data_from_server"
+PS_NEW_RESULT_FROM_NODE = "new_result_from_node"
+PS_NODE_NEEDS_MORE_DATA = "node_needs_more_data"
 
 
+def encode_message(message: Any, secret_key: bytes) -> bytes:
+    msg_ser = pickle.dumps(message)
+    msg_cmp = lzma.compress(msg_ser)
+    msg_enc = msg_cmp
+    return "Message!".encode()
 
+def decode_message(message: bytes, secret_key: bytes) -> Any:
+    # pickle.loads
+    # lzma.decompress
+    #
+    return "Message!".encode()
 
-def encode_message(message: str, secret_key: str) -> str:
-    return "Message!"
+def ps_gen_heartbeat_message(node_id: PSNodeId, secret_key: bytes) -> bytes:
+    msg = (PS_HEARTBEAT_MESSAGE, node_id)
+    return encode_message(msg, secret_key)
 
-def decode_message(message: str, secret_key: str) -> str:
-    return "Message!"
+def ps_gen_init_message(node_id: PSNodeId, secret_key: bytes) -> bytes:
+    msg = (PS_INIT_MESSAGE, node_id)
+    return encode_message(msg, secret_key)
 
-def ps_gen_heartbeat_message(noed_id: PSNodeId, secret_key: str) -> str:
-    return "Hello!"
+def ps_gen_result_message(node_id: PSNodeId, secret_key: bytes, new_data: Any) -> bytes:
+    msg = (PS_NEW_RESULT_FROM_NODE, node_id, new_data)
+    return encode_message(msg, secret_key)
 
-def ps_gen_init_message(node_id: PSNodeId, secret_key: str) -> str:
-    return "Init!"
-
-def ps_gen_result_message(node_id: PSNodeId, secret_key: str, new_data: Any) -> str:
-    return "Result!"
-
-def ps_gen_need_more_data_message(node_id: PSNodeId, secret_key: str) -> str:
-    return "Need Data!"
+def ps_gen_need_more_data_message(node_id: PSNodeId, secret_key: bytes) -> bytes:
+    msg = (PS_NODE_NEEDS_MORE_DATA, node_id)
+    return encode_message(msg, secret_key)
 
 
 
