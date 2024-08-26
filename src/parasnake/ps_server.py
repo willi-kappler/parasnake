2ea0a7c999d7d652b8c1757fdd98522c9855ab72
Willi Kappler <willi.kappler.gm@gmail.com>
2024-08-26 19:54:14 +0200

    First empty class implementation.

---
+++ src/parasnake/ps_server.py
@@ -0,0 +1,22 @@
+# This file is part of Parasnake, a distributed number crunching library for Python
+# written by Willi Kappler, MIT license.
+#
+# See: https://github.com/willi-kappler/parasnake
+
+"""
+This module defines all the server class that distributes the work load to each node.
+"""
+
+from parasnake.ps_config import PSConfiguration
+from parasnake.ps_nodeid import PSNodeId
+import parasnake.ps_message as ps_msg
+
+
+class PDServer:
+    def __init__(self, configuration: PSConfiguration):
+        pass
+
+
+
+
+
