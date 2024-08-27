cbbdc7344eee5d5b44162df185338b5e98d7abc2
Willi Kappler <willi.kappler.gm@gmail.com>
2024-08-27 23:45:52 +0200

    Add helper method for connecting and sending. Fix communication: every send is a new connect.

--- src/parasnake/ps_node.py
+++ src/parasnake/ps_node.py
@@ -27,8 +27,8 @@ class PSNode:
         self.node_id: PSNodeId = PSNodeId()
 
     def ps_run(self):
-        logger.info("Starting node with node id: {self.node_id}")
-        logger.debug(f"Server address: {self.server_address}, port: {self.server_port}")
+        logger.info("Starting node with node id: {self.node_id}.")
+        logger.debug(f"Server address: {self.server_address}, port: {self.server_port}.")
         logger.debug(f"Heartbeat timeout: {self.heartbeat_timeout}")
 
         asyncio.run(self.ps_start_tasks())
@@ -40,35 +40,59 @@ class PSNode:
             main_task = tg.create_task(self.ps_main_loop())
             heartbeat_task = tg.create_task(self.ps_send_heartbeat())
 
-    async def ps_main_loop(self):
-        logger.debug("Start main task.")
-
+    async def ps_send_msg_return_answer(self, msg: bytes) -> Any:
+        logger.debug("Send message to server.")
         reader, writer = await asyncio.open_connection(self.server_address, self.server_port)
 
-        logger.debug("Send init message to server...")
-        writer.write(ps_msg.ps_gen_init_message(self.node_id, self.secret_key))
+        writer.write(msg)
         await writer.drain()
+        writer.close()
+        await writer.wait_closed()
 
+        data = await reader.read()
+        msg = ps_msg.decode_message(data, self.secret_key)
+        logger.debug("Received message from server.")
+
+        return msg
+
+    async def ps_main_loop(self):
+        logger.debug("Start main task.")
         need_more_data_message = ps_msg.ps_gen_need_more_data_message(self.node_id, self.secret_key)
+        init_message = ps_msg.ps_gen_init_message(self.node_id, self.secret_key)
+
+        msg = self.ps_send_msg_return_answer(init_message)
+
+        match msg:
+            case (ps_msg.PS_INIT_OK, data):
+                logger.debug("Init node OK.")
+                self.ps_init(data)
+            case ps_msg.PS_INIT_ERROR:
+                logger.error("Init node failed!")
+                return
+            case ps_msg.PS_QUIT:
+                logger.debug("Job finished.")
+                return
+            case _:
+                logger.error("Received unknown message from server!")
+                return
 
         while True:
-            data = await reader.read()
-            msg = ps_msg.decode_message(data, self.secret_key)
-            logger.debug("Received message from server.")
+            msg = self.ps_send_msg_return_answer(need_more_data_message)
 
             match msg:
-                case (ps_msg.PS_INIT_OK, data):
-                    logger.debug("Init node OK.")
-                    self.ps_init(data)
-                case ps_msg.PS_INIT_ERROR:
-                    logger.error("Init node failed!")
-                    break
                 case (ps_msg.PS_NEW_DATA_FROM_SERVER, new_data):
                     logger.debug("Received new data from server.")
                     new_result = self.ps_process_data(new_data)
-                    writer.write(ps_msg.ps_gen_result_message(self.node_id, self.secret_key, new_result))
-                    logger.debug("New processed data has been sent to server.")
-                    await writer.drain()
+                    logger.debug("New data has been processed.")
+                    result_msg = ps_msg.ps_gen_result_message(self.node_id, self.secret_key, new_result)
+                    msg = self.ps_send_msg_return_answer(result_msg)
+
+                    match msg:
+                        case ps_msg.PS_RESULT_OK:
+                            logger.debug("New processed data has been sent to server.")
+                        case _:
+                            logger.error("Received unknown message from server!")
+                            break
                 case ps_msg.PS_QUIT:
                     logger.debug("Job finished.")
                     break
@@ -76,28 +100,15 @@ class PSNode:
                     logger.error("Received unknown message from server!")
                     break
 
-            writer.write(need_more_data_message)
-            await writer.drain()
-
-        writer.close()
-        await writer.wait_closed()
-
     async def ps_send_heartbeat(self):
         logger.debug("Start heartbeat task.")
-
-        reader, writer = await asyncio.open_connection(self.server_address, self.server_port)
         heartbeat_message = ps_msg.ps_gen_heartbeat_message(self.node_id, self.secret_key)
 
         while True:
             await asyncio.sleep(self.heartbeat_timeout)
 
             logger.debug("Send heartbeat message to server.")
-            writer.write(heartbeat_message)
-            await writer.drain()
-
-            data = await reader.read()
-            msg = ps_msg.decode_message(data, self.secret_key)
-            logger.debug("Received message from server.")
+            msg = self.ps_send_msg_return_answer(heartbeat_message)
 
             match msg:
                 case ps_msg.PS_HEARTBEAT_OK:
@@ -112,13 +123,12 @@ class PSNode:
                     logger.error("Received unknown message from server!")
                     break
 
-        writer.close()
-        await writer.wait_closed()
-
     def ps_init(self, data: Any):
+        # Must be implemented by the user.
         pass
 
     def ps_process_data(self, data: Any) -> Any:
+        # Must be implemented by the user.
         pass
 
 
