800043083caa15e658082c2cc554f5d32008233b
Willi Kappler <willi.kappler.gm@gmail.com>
2024-08-28 13:38:15 +0200

    Initial code for testing configuration.

---
+++ tests/test_config.py
@@ -0,0 +1,19 @@
+# This file is part of Parasnake, a distributed number crunching library for Python
+# written by Willi Kappler, MIT license.
+#
+# See: https://github.com/willi-kappler/parasnake
+
+import unittest
+
+from parasnake.ps_config import PSConfiguration
+
+
+class TestNodeId(unittest.TestCase):
+    def test_(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
