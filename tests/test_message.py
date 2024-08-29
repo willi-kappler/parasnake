a56ca7b3172bcd4e6034960156d38df88e28d93e
Willi Kappler <willi.kappler.gm@gmail.com>
2024-08-29 13:01:57 +0200

    Add initial code for testing message functions.

---
+++ tests/test_message.py
@@ -0,0 +1,18 @@
+# This file is part of Parasnake, a distributed number crunching library for Python
+# written by Willi Kappler, MIT license.
+#
+# See: https://github.com/willi-kappler/parasnake
+
+import unittest
+
+import parasnake.ps_message as psm
+
+
+class TestMessage(unittest.TestCase):
+    pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
