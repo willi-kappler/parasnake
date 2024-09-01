8cd1aa393dff1c147a93e5c9e59aa8436bff9677
Willi Kappler <willi.kappler.gm@gmail.com>
2024-09-01 18:09:35 +0200

    Remove process pool executor, use async code to return controll to event loop.

--- src/parasnake/ps_server.py
+++ src/parasnake/ps_server.py
@@ -10,7 +10,6 @@ This module defines all the server class that distributes the work load to each 
 # Python std modules:
 import time
 import asyncio
-from concurrent.futures import ProcessPoolExecutor
 from typing import Any, Optional
 import logging
 
@@ -59,58 +58,49 @@ class PSServer:
     async def ps_handle_node(self, reader, writer) -> None:
         logger.debug("Connection from node.")
 
-        # https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.loop.run_in_executor
-        loop = asyncio.get_running_loop()
-
         data = await reader.read()
         msg = psm.decode_message(data, self.secret_key)
-
-        #logger.debug("Message decoded.")
 
         if self.quit:
             logger.debug("Send quit message, job is done.")
             await self.ps_write_msg(writer, psm.ps_gen_quit_message(self.secret_key))
         else:
-            with ProcessPoolExecutor() as pool:
-                match msg:
-                    case (psm.PS_INIT_MESSAGE, node_id):
-                        logger.debug("Init message.")
-                        if node_id in self.all_nodes:
-                            logger.error("Node id already registered: {node_id}")
-                            await self.ps_write_msg(writer, psm.ps_gen_init_message_error(self.secret_key))
-                        else:
-                            self.ps_register_new_node(node_id)
-                            init_data = await loop.run_in_executor(pool, self.ps_get_init_data, node_id)
-                            #init_data = self.ps_get_init_data(node_id)
-                            await self.ps_write_msg(writer, psm.ps_gen_init_message_ok(init_data, self.secret_key))
-                    case (psm.PS_HEARTBEAT_MESSAGE, node_id):
-                        logger.debug("Heartbeat message.")
-                        if node_id in self.all_nodes:
-                            self.ps_update_node_time(node_id)
-                            await self.ps_write_msg(writer, psm.ps_gen_heartbeat_message_ok(self.secret_key))
-                        else:
-                            logger.error("Node id not registered yet: {node_id}")
-                            await self.ps_write_msg(writer, psm.ps_gen_heartbeat_message_error(self.secret_key))
-                    case (psm.PS_NODE_NEEDS_MORE_DATA, node_id):
-                        logger.debug("Node needs more data.")
-                        if node_id in self.all_nodes:
-                            self.ps_update_node_time(node_id)
-                            new_data = await loop.run_in_executor(pool, self.ps_get_new_data, node_id)
-                            #new_data = self.ps_get_new_data(node_id)
-                            await self.ps_write_msg(writer, psm.ps_gen_new_data_message(new_data, self.secret_key))
-                        else:
-                            logger.error("Node id note registered yet: {node_id}")
-                            await self.ps_write_msg(writer, psm.ps_gen_init_message_error(self.secret_key))
-                    case (psm.PS_NEW_RESULT_FROM_NODE, node_id, result):
-                        logger.debug("New result from node.")
-                        if node_id in self.all_nodes:
-                            self.ps_update_node_time(node_id)
-                            await loop.run_in_executor(pool, self.ps_process_result, node_id, result)
-                            #self.ps_process_result(node_id, result)
-                            await self.ps_write_msg(writer, psm.ps_gen_result_ok_message(self.secret_key))
-                        else:
-                            logger.error("Node id note registered yet: {node_id}")
-                            await self.ps_write_msg(writer, psm.ps_gen_init_message_error(self.secret_key))
+            match msg:
+                case (psm.PS_INIT_MESSAGE, node_id):
+                    logger.debug("Init message.")
+                    if node_id in self.all_nodes:
+                        logger.error("Node id already registered: {node_id}")
+                        await self.ps_write_msg(writer, psm.ps_gen_init_message_error(self.secret_key))
+                    else:
+                        self.ps_register_new_node(node_id)
+                        init_data = await self.ps_get_init_data(node_id)
+                        await self.ps_write_msg(writer, psm.ps_gen_init_message_ok(init_data, self.secret_key))
+                case (psm.PS_HEARTBEAT_MESSAGE, node_id):
+                    logger.debug("Heartbeat message.")
+                    if node_id in self.all_nodes:
+                        self.ps_update_node_time(node_id)
+                        await self.ps_write_msg(writer, psm.ps_gen_heartbeat_message_ok(self.secret_key))
+                    else:
+                        logger.error("Node id not registered yet: {node_id}")
+                        await self.ps_write_msg(writer, psm.ps_gen_heartbeat_message_error(self.secret_key))
+                case (psm.PS_NODE_NEEDS_MORE_DATA, node_id):
+                    logger.debug("Node needs more data.")
+                    if node_id in self.all_nodes:
+                        self.ps_update_node_time(node_id)
+                        new_data = await self.ps_get_new_data(node_id)
+                        await self.ps_write_msg(writer, psm.ps_gen_new_data_message(new_data, self.secret_key))
+                    else:
+                        logger.error("Node id note registered yet: {node_id}")
+                        await self.ps_write_msg(writer, psm.ps_gen_init_message_error(self.secret_key))
+                case (psm.PS_NEW_RESULT_FROM_NODE, node_id, result):
+                    logger.debug("New result from node.")
+                    if node_id in self.all_nodes:
+                        self.ps_update_node_time(node_id)
+                        await self.ps_process_result(node_id, result)
+                        await self.ps_write_msg(writer, psm.ps_gen_result_ok_message(self.secret_key))
+                    else:
+                        logger.error("Node id note registered yet: {node_id}")
+                        await self.ps_write_msg(writer, psm.ps_gen_init_message_error(self.secret_key))
 
         writer.close()
         await writer.wait_closed()
@@ -149,7 +139,7 @@ class PSServer:
             if int(now - v + 1.0) > self.heartbeat_timeout:
                 self.ps_node_timeout(k)
 
-    def ps_get_init_data(self, node_id: PSNodeId) -> Any:
+    async def ps_get_init_data(self, node_id: PSNodeId) -> Any:
         # Must be implemented by the user.
         return None
 
@@ -165,11 +155,11 @@ class PSServer:
         # Must be implemented by the user.
         pass
 
-    def ps_get_new_data(self, node_id: PSNodeId) -> Optional[Any]:
+    async def ps_get_new_data(self, node_id: PSNodeId) -> Optional[Any]:
         # Must be implemented by the user.
         return None
 
-    def ps_process_result(self, node_id: PSNodeId, result: Any):
+    async def ps_process_result(self, node_id: PSNodeId, result: Any):
         # Must be implemented by the user.
         pass
 
