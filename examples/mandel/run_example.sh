f1a3b6ed8253a2c86b1dbdb71cae053549179736
Willi Kappler <willi.kappler.gm@gmail.com>
2024-09-19 22:39:24 +0200

    Add example run script.

---
+++ examples/mandel/run_example.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+reset
+
+export PYTHONPATH=$PYTHONPATH:"../../src/"
+
+python3 main.py --server &
+sleep 2
+python3 main.py &
+sleep 2
+python3 main.py &
+sleep 2
+python3 main.py &
+sleep 2
+python3 main.py &
+sleep 2
+
