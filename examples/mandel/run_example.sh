40459e402b8ea367aa66080815a74943b5a0320e
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-09-20 12:16:48 +0200

    Enable all 4 nodes again.

--- examples/mandel/run_example.sh
+++ examples/mandel/run_example.sh
@@ -7,10 +7,10 @@ export PYTHONPATH=$PYTHONPATH:"../../src/"
 python3 main.py --server &
 sleep 2
 python3 main.py &
-#sleep 2
-#python3 main.py &
-#sleep 2
-#python3 main.py &
-#sleep 2
-#python3 main.py &
+sleep 2
+python3 main.py &
+sleep 2
+python3 main.py &
+sleep 2
+python3 main.py &
 
