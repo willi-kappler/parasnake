21f76850b5a3fc03ed5b5aafcd2151e76f81f669
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-09-20 16:55:04 +0200

    Add checks for mandel example.

--- tests/check_all.sh
+++ tests/check_all.sh
@@ -7,21 +7,27 @@ export PYTHONPATH=$PYTHONPATH:"src/"
 echo "ruff:"
 ruff check src/parasnake/
 ruff check tests/
+ruff check examples/mandel/
 
 echo -e "\n\n-------------------------------------------\n\n"
 
 echo "mypy:"
 mypy --check-untyped-defs src/parasnake/
 mypy --check-untyped-defs tests/
+mypy --check-untyped-defs examples/mandel/
+
 
 echo -e "\n\n-------------------------------------------\n\n"
 
 echo "flake8:"
 flake8 src/parasnake/
 flake8 tests/
+flake8 examples/mandel/
 
 echo -e "\n\n-------------------------------------------\n\n"
 
 echo "pyright:"
 pyright src/parasnake/
 pyright tests/
+pyright examples/mandel/
+
